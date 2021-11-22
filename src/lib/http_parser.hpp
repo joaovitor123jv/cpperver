a1e9600e7b4dabb21e7dcc93231d714f92f70823
João Vitor Antoniassi Segantin <segantin.jv@gmail.com>
2021-11-22 06:46:48 -0300

    WIP: HttpParser

---
+++ src/lib/http_parser.hpp
@@ -0,0 +1,11 @@
+#ifndef __CERVER_HTTP_PARSER
+#define __CERVER_HTTP_PARSER
+
+// Here will be defined a HTTP request parser, to identify items on HTTP headers and so on...
+
+class HttpParser {
+    private:
+    public:
+};
+
+#endif // __CERVER_HTTP_PARSER
